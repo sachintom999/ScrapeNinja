deacc70c9dc58ff30fe6f016086d023521126979
Sachin Tom <sachin.tom.dev@gmail.com>
2025-03-17 12:23:02 +0530

    checkpoint-1: worker-manager also working, scaling seems to be working, need to confirm

---
+++ docker_api.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+import subprocess
+
+app = Flask(__name__)
+
+@app.route("/scale", methods=["POST"])
+def scale():
+    num_workers = request.json.get("num_workers", 1)
+    # subprocess.run(["docker", "compose", "up", "--scale", f"my-scraper-1={num_workers}"])
+    subprocess.run(["docker", "compose", "up", "-d", "--scale", f"my-scraper-1={num_workers}"])
+
+    return {"status": "success", "scaled_to": num_workers}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
--- worker-manager/main.py
+++ worker-manager/main.py
@@ -21,11 +21,25 @@ def get_queue_size():
             time.sleep(5)
     raise Exception("Kafka is still not available after retries")
 
+# def scale_workers():
+#     queue_size = get_queue_size()
+#     num_workers = queue_size // 10  # 1 worker per 10 requests
+#     # subprocess.run(["docker-compose", "up", "--scale", f"scraper-service={num_workers}"])
+#     subprocess.run(["docker", "compose", "up", "--scale", f"scraper-service={num_workers}"])
+
+
+
+# if __name__ == "__main__":
+#     scale_workers()
+
+
+
+import requests
+
 def scale_workers():
     queue_size = get_queue_size()
     num_workers = queue_size // 10  # 1 worker per 10 requests
-    subprocess.run(["docker-compose", "up", "--scale", f"scraper-service={num_workers}"])
-
+    requests.post("http://host.docker.internal:5001/scale", json={"num_workers": num_workers})
 
 if __name__ == "__main__":
     scale_workers()
